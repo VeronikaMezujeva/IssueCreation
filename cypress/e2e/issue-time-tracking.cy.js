e1b3e54f994235e9dea5da667fbcc2381d5a0177
cy.get('[data-testid=select:type]').then(($typeValue) <veronika.mezujeva@gmail.com>
2023-10-03 14:21:44 +0300

    My comments

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,102 +1,74 @@
 import { faker } from '@faker-js/faker';
-const title = faker.lorem.word();
-const description = faker.lorem.words();
+
+const title = faker.lorem.words({ min: 2, max: 5 });
+const description = faker.lorem.sentence();
+const estimatedHours = '10';
+const updatedEstimatedHours = '20';
 
 describe('Time tracking', () => {
     beforeEach(() => {
         cy.visit('/');
         cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
             cy.visit(url + '/board?modal-issue-create=true');
+            inputCreateIssue()
         });
     });
+  });
 
-    it('assert that issue is created, add time estimation and validate it', () => {
-        const getIssueCreateModal = () => cy.get('[data-testid="modal:issue-create"]');
+    it('Add, edit and remove time estimation', () => {
         const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
 
-        //Assert that issue is successfully created
-        getIssueDetailsModal().within(() => {
-            cy.get('.ql-container').type(description);
-            cy.get('input[name="title"]').type(title);
-            cy.get('[data-testid="select:type"]').click();
-            cy.get('[data-testid="icon:bug"]').trigger('click');
-            cy.get('[data-testid="select:userIds"]').click();
-            cy.get('[data-testid="select-option:Pickle Rick"]').click();
-            cy.get('[data-testid="select:priority"]').click();
-            cy.get('[data-testid="select-option:Highest"]').click();
-            cy.get('button[type="submit"]').click();
-            cy.get('[data-testid="modal:issue-create"]').should('not.exist');
-            cy.contains('Issue has been successfully created.').should('be.visible');
-            cy.reload();
-            cy.contains('Issue has been successfully created.').should('not.exist');
-            cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-                cy.get('[data-testid="list-issue"]')
-                    .should('have.length', '5')
-                    .first()
-                    .find('p')
-                    .contains(title);
-                cy.get('[data-testid="avatar:Pickle Rick"]').eq(0).should('be.visible');
-                cy.get('[data-testid="icon:arrow-up"]').eq(0).should('be.visible');
-                cy.get('[data-testid="icon:bug"]').eq(0).should('be.visible');
-            });      
-
         //Add time estimation
-        cy.get('[data-testid="board-list:backlog').should('have.length', '1').within(() => {
-            cy.get('[data-testid="list-issue"]')
-                .should('have.length', '5').first().click();
+        getIssueDetailsModal().within(() => {
             cy.contains('No time logged').should('exist');
-            cy.get('[placeholder="Number"]').type(10);
-            cy.contains('10h estimated');
+            cy.get('input[placeholder="Number"]').should('be.empty').click().type(estimatedHours);
+            cy.contains(`${estimatedHours}h estimated`);
             cy.get('[data-testid="icon:close"]').first().click();
-        });
-        //Assert that estimated time is successfully added
-        cy.get('[data-testid="modal:issue-details"]').should('not.exist');
-        cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-            cy.get('[data-testid="list-issue"]')
-                .should('have.length', '5').first().click();
 
-            cy.get('[placeholder="Number"]').contains(10);
-            cy.get('[data-testid="modal:issue-details"]').contains('10h estimated');
+            //Assert that estimated time is successfully added
+            cy.get('[data-testid="board-list:backlog').contains(title).click();
+            cy.get('input[placeholder="Number"]').should('have.value', estimatedHours);
+            cy.get('[data-testid="modal:issue-details"]').contains(`${estimatedHours}h estimated`);
             cy.get('[data-testid="icon:close"]').first().click();
-        });
-        //Edit time estimation
-        cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-            cy.get('[data-testid="list-issue"]')
-                .should('have.length', '5').first().click();
 
-            cy.get('[placeholder="Number"]').click().clear();
-            cy.get('[placeholder="Number"]').type(20);
-            cy.get('[data-testid="modal:issue-details"]').should('contain', '20h estimated');
+            //Edit time estimation
+            cy.get('[data-testid="board-list:backlog').contains(title).click();
+            cy.get('input[placeholder="Number"]').should('have.value', estimatedHours).click().clear();
+            cy.get('input[placeholder="Number"]').type(updatedEstimatedHours);
+            cy.get('[data-testid="modal:issue-details"]').contains(`${updatedEstimatedHours}h estimated`);
             cy.get('[data-testid="icon:close"]').first().click();
-        });
-        //Assert that estimated time is updated
-        cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-            cy.get('[data-testid="list-issue"]')
-            cy.should('have.length', '5').first().click();
-        });
-        cy.get('[data-testid="modal:issue-details"]').should('be.visible').within(() => {
-            cy.get('[data-testid="icon:stopwatch"]')
-            cy.contains('20h estimated');
-            cy.get('[data-testid="icon:close"]').first().click();
-        });
-        //Remove time estimation
-        cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-            cy.get('[data-testid="list-issue"]')
-                .should('have.length', '5').first().click();
-        });
-        cy.get('[data-testid="modal:issue-details"]').should('be.visible').within(() => {
-            cy.get('[placeholder="Number"]').click().clear();
-            cy.log
-            cy.contains('20h estimated').should('not.exist');
-            cy.get('[data-testid="icon:close"]').first().click();
-        });
-        //Assert that estimated time is removed
-        cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-            cy.get('[data-testid="list-issue"]')
-                .should('have.length', '5').first().click();
 
+            //Assert that estimated time is updated
+            cy.get('[data-testid="board-list:backlog').contains(title).click();
+            cy.get('input[placeholder="Number"]').should('have.value', updatedEstimatedHours);
+            cy.get('[data-testid="modal:issue-details"]').contains;
+            cy.get('[data-testid="icon:close"]').first().click();
+
+            //Remove time estimation
+            cy.get('[data-testid="board-list:backlog').contains(title).click();
+            cy.get('[data-testid="modal:issue-details"]').should('be.visible').within(() => {
+                cy.get('input[placeholder="Number"]').click().clear();
+                cy.log
+                cy.contains(`${updatedEstimatedHours}h estimated`).should('not.exist');
+                cy.get('[data-testid="icon:close"]').first().click();
+            });
+            //Assert that estimated time is removed
+            cy.get('[data-testid="board-list:backlog').contains(title).click();
             cy.get('[data-testid="modal:issue-details"]').should('be.visible');
-            cy.get('[placeholder="Number"]').should('have.value', '');
-    
+            cy.get('input[placeholder="Number"]').should('have.value', '');
+            cy.contains('No time logged').should('exist');
+            cy.get('[data-testid="icon:close"]').first().click();
         });
     });
+
+    function inputCreateIssue() {
+        cy.get('.ql-container').type(description)
+        cy.get('input[name="title"]').type(title)
+        cy.get('[data-testid="select:type"]').click();
+        cy.get('[data-testid="icon:bug"]').trigger('click')
+        cy.get('[data-testid="select:userIds"]').click()
+        cy.get('[data-testid="select-option:Pickle Rick"]').click()
+        cy.get('[data-testid="select:priority"]').click()
+        cy.get('[data-testid="select-option:Highest"]').click()
+        cy.get('button[type="submit"]').click()
+    };
